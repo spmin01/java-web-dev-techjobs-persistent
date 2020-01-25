41db6a8bd136f9ab01e6b189d3b3d863d50924bc
Shaun Minnihan <spminnihan@gmail.com>
2020-01-25 13:04:48 -0600

    Refactored to use AbstractEntity and added data layer for Skill and Employer

--- src/main/java/org/launchcode/javawebdevtechjobspersistent/models/Skill.java
+++ src/main/java/org/launchcode/javawebdevtechjobspersistent/models/Skill.java
@@ -1,5 +1,23 @@
 package org.launchcode.javawebdevtechjobspersistent.models;
 
+import javax.persistence.Entity;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
+
+@Entity
 public class Skill extends AbstractEntity {
 
+    @NotBlank(message ="Skill description field must not be blank.")
+    @Size(min = 3, max=255, message="Description must be between 3 and 255 characters")
+    private String skillDescription;
+
+    public Skill() {}
+
+    public String getSkillDescription() {
+        return skillDescription;
+    }
+
+    public void setSkillDescription(String skillDescription) {
+        this.skillDescription = skillDescription;
+    }
 }
