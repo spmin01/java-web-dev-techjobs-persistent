93d7baba98092d3b00380e1c43f5219144a8febc
Shaun Minnihan <spminnihan@gmail.com>
2020-01-25 13:15:37 -0600

    submit employer sorta works! (it's 404ing, why?!?)

--- src/main/java/org/launchcode/javawebdevtechjobspersistent/models/Job.java
+++ src/main/java/org/launchcode/javawebdevtechjobspersistent/models/Job.java
@@ -3,30 +3,49 @@ package org.launchcode.javawebdevtechjobspersistent.models;
 import javax.persistence.*;
 
 @Entity
-public class Job extends AbstractEntity {
+public class Job {
 
-    private Employer employer;
-    private Skill skills;
+    @Id
+    @GeneratedValue
+    private int id;
+
+    private String name;
+
+    private String employer;
+    private String skills;
 
     public Job() {
     }
 
+    public Job(String anEmployer, String someSkills) {
+        super();
+        this.employer = anEmployer;
+        this.skills = someSkills;
+    }
 
     // Getters and setters.
 
-    public Employer getEmployer() {
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmployer() {
         return employer;
     }
 
-    public void setEmployer(Employer employer) {
+    public void setEmployer(String employer) {
         this.employer = employer;
     }
 
-    public Skill getSkills() {
+    public String getSkills() {
         return skills;
     }
 
-    public void setSkills(Skill skills) {
+    public void setSkills(String skills) {
         this.skills = skills;
     }
 }
