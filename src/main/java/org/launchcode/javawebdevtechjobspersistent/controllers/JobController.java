132e86f63219d43be69c4eec5ab49d190fbbf899
Shaun Minnihan <spminnihan@gmail.com>
2020-02-29 20:28:04 -0600

    added GET requests by skills

--- src/main/java/org/launchcode/javawebdevtechjobspersistent/controllers/JobController.java
+++ src/main/java/org/launchcode/javawebdevtechjobspersistent/controllers/JobController.java
@@ -1,13 +1,16 @@
 package org.launchcode.javawebdevtechjobspersistent.controllers;
 
+import org.launchcode.javawebdevtechjobspersistent.models.Job;
 import org.launchcode.javawebdevtechjobspersistent.models.JobData;
-import org.launchcode.javawebdevtechjobspersistent.models.data.EmployerRepository;
+import org.launchcode.javawebdevtechjobspersistent.models.Skill;
 import org.launchcode.javawebdevtechjobspersistent.models.data.JobRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Optional;
 
 @RestController
@@ -18,9 +21,27 @@ public class JobController {
     JobRepository jobRepository;
 
     @GetMapping
-    public ResponseEntity getJobByEmployer(@RequestParam Optional<String> employer) {
-        if(employer.isPresent()) {
+    public ResponseEntity getJobByEmployer(@RequestParam Optional<String> employer, @RequestParam Optional<List<String>> skills) {
+
+        if(employer.isPresent() && !skills.isPresent()) {
             return new ResponseEntity(JobData.findByValue(employer.get(), jobRepository.findAll()), HttpStatus.OK);
+
+        } else if (!employer.isPresent() && skills.isPresent()) {
+            List<Job> foundJobs = new ArrayList<>();
+            for (String skill : skills.get()) {
+                foundJobs.addAll(JobData.findByValue(skill, jobRepository.findAll()));
+            }
+            return new ResponseEntity(foundJobs, HttpStatus.OK);
+
+        } else if(employer.isPresent() && skills.isPresent()) {
+            List<Job> employerJobs = JobData.findByValue(employer.get(), jobRepository.findAll());
+            List<Job> jobsMatchingSkills = new ArrayList<>();
+
+            for (String skill : skills.get()) {
+                jobsMatchingSkills.addAll(JobData.findByValue(skill, employerJobs));
+            }
+            return new ResponseEntity(jobsMatchingSkills, HttpStatus.OK);
+
         } else {
             return new ResponseEntity(jobRepository.findAll(), HttpStatus.OK);
         }
@@ -35,5 +56,11 @@ public class JobController {
         }
 
     }
+
+//    @DeleteMapping("/{id}")
+//    public ResponseEntity deleteJobById(@PathVariable("id"), int id) {
+//
+//    }
+
 
 }
